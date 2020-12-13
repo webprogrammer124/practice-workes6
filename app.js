c7ce5dee1ea4ace36c1f60274a21e0c097cb7fdf
Seema Yousuf Zai <72159369+webprogrammer124@users.noreply.github.com>
2020-12-13 14:23:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,197 @@
+//advance 16 nov 20//
+//if(true){
+    //let X=1:
+
+
+//let age = prompt("What is your age?", 18);
+
+//let welcome = (age < 18) ?
+ // () => alert('Hello') :
+ // () => alert("Greetings!");
+ //let multi = function(a, b) {
+   // return a * b;
+ // };
+  
+  
+  //alert( mult(1, 2) ); // 3 
+  //const multiply =()=>{
+     // var c =2*2;
+     // return (c);}
+  //console.log(multiply());
+  //map funtion saquare//
+ // var arr =[ 4,9,16,25];
+  //const newArr = arr.map(
+     //(item) =>{
+       //   return Math.sqrt(item)
+      //}
+
+ // )
+ // console.log(newArr)
+  //for greater number//
+  //var arr = [2,4,6,7];
+ // const newArr = arr.filter(
+     // (item) =>{
+        //  return item > 2
+     // }
+ // ) 
+ // console.log(newArr)
+ //17 nov 2020//
+// [a,b, c, d, ...e] = [10, 20,30,40,5 ,7,8];
+ //console.log(d)
+ //[a,b, c, d, ...e] = [10, 20,30,40,5 ,7,8];
+ //console.log(e)
+ //class task//
+ //var teachers =['missfaiza','sir ali','sir mughal']
+ //var ages = [20,18,19,21,23];
+ //var studnts = ['simee','bela','fati']
+ //let find = teachers.find((item)=>{
+  // return item =='missfaiza'
+ //})
+ //if(find){
+  // let res = studnts.map((item,index)=>{
+   //  return {name:item, age:ages[index]}
+   //})
+  //  console.log(res)
+ //}//terninat
+ //let age = 18;
+  //var cnic =( age>=18)?'yes': 'no'
+  //console.log(cnic)
+ // var str ='after break'
+ /// var learn =(str == 'after break')?'no':'yes'
+  //console.log(learn)
+  //class task//
+  //var salary = 50000
+ // var bounus =( salary>=5000)?'1000':'100'
+  //console.log(bounus)
+ // var allowed = (age>18) ?'yes'
+            //   :(age == 18)? 'let me think'
+             //  :(age == 18)? 'let me think' 
+          //     :invalid
+ // console.log(allowed)             
+//var per = 80
+//var solarship = (per>=80)?'you have solarship':
+//18 n0v 20 spread operator//
+
+//var arr =[2,3,4,5]
+//var arr2 = arr;
+//arr2[0] = 4;
+//console.log(arr)
+
+ // var arr = [2,3,4,5]
+ // var arr1 = [6,7,8,9]
+ // var arr2 = [...arr]
+ // arr2[0] =4
+ // console.log(arr2)
+  //concat//
+  //var arr = [2,3,4,5]
+  //var arr3 =[...arr,...arr1]
+  //console.log(arr3)
+ //object//
+// var obj = {name:'faiza'}
+ //var obj = {  name:'faiza'}
+//var obj2 = {  name:'faiza'}
+//var objNew = {...obj};
+// objNew.deal = 2;
+ //console.log(obj)
+ //function sum(x,y,z){
+    //return x+y+z
+ //}
+ //sum(3,5,6);
+ //let arr4 = [3,5,6]
+ //var res = sum(...arr4)
+ //console.log(res)
+ //how to add arrow in rest//
+// let sum = (a, ...b) =>{
+//  var res1 = b.reduce((item,index) =>{
+//   return item + index
+   //  })
+   //  rest1 = res1 + a;
+   //  minus a - res1
+   //  console.log(res1)
+   //  return res1
+//  }
+// var res = sum(1,2,3,5,6)
+//let[a, b, c, d,...e] = [10,20,40,5,6,7]
+//function check(...op){
+   //console.log(op)
+   //let [a,b,c] = op
+   //console.log(a,b,c)
+//}
+ //check(1,2,3)
+ //destructuring.//
+//  var obj = { name:'seemi',status:'active'}
+//  var arr = [3,4,5];
+//  var [a,b,c] = arr;
+//  console.log(arr)var student = {
+//spread operator object//
+// var st = { name: "bella" } 
+// var st2 = {...st2,school:"saylani"}  
+// //console.log(st2)
+//spread operator how to marge array
+
+ //var students1 = ["bella","fati"]
+//  var students2 = [...students1, "rabi","hifza"]
+//  console.log(students2)
+//  var students1 =["age","name","course",]
+//  var students2 =[ ...students1,"resutl","job"]
+//  console.log(students2)
+//object spread operator//
+// var st1 = {name:"bella"}
+//  var st2 = {...st1,school:"iu"}
+// console.log(st2)
+//object destructuring//
+//  var student = {
+//   name:"fat",
+//   age:12,
+//   roll:123,
+// }
+//  let{name,roll,school} = student;
+//  console.log(student.name.age.roll)//yeha jo console hugaye wohi meelaai gaye//
+//question Make a human object with watch, walk, eat, sleep properties//
+// var human= {
+//    eat:"more",
+//    walk:"fast",
+//    watch:"sharp",
+//    sleep:"normal",
+// } 
+// console.log(human)
+//if else condition//
+// var  bool = true;
+// var name = bool && "bella";
+// console.log(name)
+// const double=(num)=>{
+//    return num*2;
+// }
+/* <h4>You have list of students with properties
+    "name", "age" and "rollNumber".
+ Write a method to display all students
+  name and their rollnumber.
+// hint: objects n arrays */}
+// 
+// this.state={
+//    data:[{
+//       name:"kinza",
+//       age:19,
+//       rollNum:123
+// },{
+//    name:"kinza",
+//       age:19,
+//       rollNum:123
+// },{name:"kinza",
+// age:19,
+// rollNum:123
+
+
+// }
+// ]
+// };
+// {!this.state.dat}
+
+
+
+
+   
+
+ 
+ 
+
